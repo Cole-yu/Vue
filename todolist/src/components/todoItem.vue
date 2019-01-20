c3004e54a7cce0aa19f5d03e24865ae0591b153d
Cole.yu <1032577646@qq.com>
2019-01-20 23:47:27 +0800

    慕课网简单vue项目

---
+++ todolist/src/components/todoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li @click="handlerDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props:["content","index"],
+  methods:{
+    handlerDelete:function(){
+      this.$emit("delete",this.index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ todolist/src/todoList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <input v-model="inputValue"/>
+    <button @click="handlerSubmit">提交</button>
+    <ul>
+      <todo-item v-for="(item,index) of items" 
+                :content="item" 
+                :key="index" 
+                :index="index" 
+                @delete="handlerDelete" >
+      </todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import todoItem from './components/todoItem';
+
+  export default {
+    components:{
+      "todo-item":todoItem
+    },
+    data:function(){
+      return{
+        inputValue:"",
+        items:[]
+      }
+    },
+    methods:{
+        handlerSubmit:function(){
+          this.items.push(this.inputValue);
+          this.inputValue="";
+        },
+        handlerDelete(index){
+          this.items.splice(index,1);
+        }
+    }
+  }
+</script>
+
+<style>
+
+</style>
