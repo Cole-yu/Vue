27315c1a1bee35799e74182642c61530c25f0a7d
Cole.yu <1032577646@qq.com>
2019-04-11 23:28:47 +0800

    上传，提示框，复合性输入框

--- test-app-management/src/views/About.vue
+++ test-app-management/src/views/About.vue
@@ -1,5 +1,134 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div id="about">
+    <h1>用户头像上传</h1>
+    <p>使用 before-upload 限制用户上传的图片格式和大小。</p>
+    <el-upload
+      class="avatar-uploader"
+      action="https://jsonplaceholder.typicode.com/posts/"
+      :show-file-list="false"
+      :on-success="handleAvatarSuccess"
+      :before-upload="beforeAvatarUpload">
+      <img v-if="imageUrl" :src="imageUrl" class="avatar">
+      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+    </el-upload>
+    
+    <hr>
+    <h1>照片墙</h1>
+    <p>使用 list-type 属性来设置文件列表的样式。</p>
+    <el-upload
+      action="https://jsonplaceholder.typicode.com/posts/"
+      list-type="picture-card"
+      :on-preview="handlePictureCardPreview"
+      :on-remove="handleRemove">
+      <i class="el-icon-plus"></i>
+    </el-upload>
+    <el-dialog :visible.sync="dialogVisible">
+      <img width="100%" :src="dialogImageUrl" alt="">
+    </el-dialog>
+
+    <hr>
+    <h1>提示框  --  更多 Content</h1>
+    <p>展示多行文本或者是设置文本内容的格式</p>
+    <!-- 四个方向：top、left、right、bottom；三种对齐位置：start, end，默认为空。如placement="left-end" -->
+    <el-tooltip placement="right-end">
+      <div slot="content">        
+        <h1>多行信息</h1>
+        <p style='color:red;font-size:30px;padding:10px;'>第二行信息</p>
+        <p>第三行信息</p>
+        <p>第四行信息</p>        
+      </div>
+      <el-button>Top center</el-button>
+    </el-tooltip>
+    
+    <hr>    
+    <h1>输入框</h1>
+    <p>复合型输入框</p>
+    <div style="margin-top: 15px;">
+      <el-input placeholder="请输入内容" v-model="input5" class="input-with-select">
+        <el-select v-model="select" slot="prepend" placeholder="请选择">
+          <el-option label="餐厅名" value="1"></el-option>
+          <el-option label="订单号" value="2"></el-option>
+          <el-option label="用户电话" value="3"></el-option>
+        </el-select>
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+    </div>
+  </div>  
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageUrl: '',
+      dialogImageUrl: '',
+      dialogVisible: false,
+      input5:'',
+      select:''
+    };
+  },
+  methods: {
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+      return isJPG && isLt2M;
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    }
+  }
+}
+</script>
+
+<style>
+  #about{
+    background-color: white;
+    padding:30px;
+  }
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+
+  /* el-input 的样式 */
+  .el-select .el-input {
+    width: 130px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+</style>
+
